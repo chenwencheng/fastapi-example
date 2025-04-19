34b1cbb04f254f4a4d649a48165a8450c5025da5
chenwencheng <18814515@qq.com>
2025-04-19 11:40:11 +0800

    update

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/test")
+async def test():
+    return {"route": "test"}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
